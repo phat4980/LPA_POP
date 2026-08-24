c0a4f4423dbb97a2e1d0deff71628506ef1d055a
hstphat <phat4980@gmail.com>
2026-08-24 12:07:42 +0700

    chore: small change for prepare 7.1

--- scripts/print.ps1
+++ scripts/print.ps1
@@ -5,14 +5,16 @@ param(
 
 $ErrorActionPreference = 'Stop'
 $sumatraPdf = Join-Path $PSScriptRoot 'vendor\sumatrapdf-3.6.1\SumatraPDF-3.6.1-64.exe'
+$quotedPrinterName = '"{0}"' -f $PrinterName.Replace('"', '\"')
+$quotedFilePath = '"{0}"' -f $FilePath.Replace('"', '\"')
 
 if (-not (Test-Path -LiteralPath $FilePath -PathType Leaf)) { throw "Print file does not exist." }
 if (-not (Test-Path -LiteralPath $sumatraPdf -PathType Leaf)) { throw "Vendored SumatraPDF executable does not exist." }
 if (-not (Get-Printer -Name $PrinterName -ErrorAction SilentlyContinue)) { throw "Configured printer does not exist." }
 
 $sumatraProcess = Start-Process -FilePath $sumatraPdf -ArgumentList @(
-  '-print-to', $PrinterName,
+  '-print-to', $quotedPrinterName,
   '-print-settings', 'simplex',
-  '-silent', $FilePath
+  '-silent', $quotedFilePath
 ) -Wait -PassThru
 if ($sumatraProcess.ExitCode -ne 0) { throw "SumatraPDF print failed with exit code $($sumatraProcess.ExitCode)." }
