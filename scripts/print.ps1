2f1fc15a17b489b0ccdd4661461c4360e333b39d
TanPhat <phat4980@gmail.com>
2026-08-24 11:29:32 +0700

    chore: update print script

--- scripts/print.ps1
+++ scripts/print.ps1
@@ -10,5 +10,9 @@ if (-not (Test-Path -LiteralPath $FilePath -PathType Leaf)) { throw "Print file 
 if (-not (Test-Path -LiteralPath $sumatraPdf -PathType Leaf)) { throw "Vendored SumatraPDF executable does not exist." }
 if (-not (Get-Printer -Name $PrinterName -ErrorAction SilentlyContinue)) { throw "Configured printer does not exist." }
 
-& $sumatraPdf -print-to $PrinterName -print-settings 'simplex' -silent $FilePath
-if ($LASTEXITCODE -ne 0) { throw "SumatraPDF print failed with exit code $LASTEXITCODE." }
+$sumatraProcess = Start-Process -FilePath $sumatraPdf -ArgumentList @(
+  '-print-to', $PrinterName,
+  '-print-settings', 'simplex',
+  '-silent', $FilePath
+) -Wait -PassThru
+if ($sumatraProcess.ExitCode -ne 0) { throw "SumatraPDF print failed with exit code $($sumatraProcess.ExitCode)." }
